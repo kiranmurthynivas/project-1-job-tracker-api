9ab9640b917d54270ac81cda58605845ff674e8b
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-28 19:25:10 +0530

    add auth protect middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+async function protect(req, res, next) {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, token missing"
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Not authorized, token failed"
+    });
+  }
+}
+
+module.exports = {
+  protect
+};

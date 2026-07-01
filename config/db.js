8db7823129717996467a120b3cabe5a6a9a3c85f
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-07-01 16:10:22 +0530

    changes for deployment in render

--- config/db.js
+++ config/db.js
@@ -10,14 +10,6 @@ async function connectDB() {
       throw new Error("MONGO_URL is missing");
     }
 
-    console.log("MongoDB connection string configured");
-    const parsedUrl = new URL(mongoUrl);
-
-    console.log("MongoDB URL protocol:", parsedUrl.protocol);
-    console.log("MongoDB URL username:", parsedUrl.username);
-    console.log("MongoDB URL host:", parsedUrl.host);
-    console.log("MongoDB URL database:", parsedUrl.pathname);
-    console.log("MongoDB password length:", parsedUrl.password.length);
     await mongoose.connect(mongoUrl);
     console.log("MongoDB connected successfully");
   } catch (error) {
